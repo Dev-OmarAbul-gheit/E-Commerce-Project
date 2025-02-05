2fe17c21d6b64c76d930aeddcf3347083383cdb1
Dev, Omar Abul-gheit <dev.omarabulgheit@gmail.com>
2025-02-05 18:44:55 +0200

    Added a new endpoint to the <Customer> endpoints to allow authenticated user to view and update their customer record.

--- store/views.py
+++ store/views.py
@@ -1,6 +1,8 @@
+from rest_framework.response import Response
 from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin
 from rest_framework.viewsets import GenericViewSet, ModelViewSet
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.decorators import action
 from .models import User, Collection, Product, ProductImage, Customer
 from .serializers import (SignUpSerializer, SignInSerializer,
                           UserSerializer, CollectionSerializer,
@@ -53,3 +55,16 @@ class CustomerViewSet(ModelViewSet):
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
     permission_classes = [IsAdminUser]
+
+    @action(detail=False,  methods= ['GET', 'PUT'], permission_classes = [IsAuthenticated])
+    def me(self, request):
+        customer = Customer.objects.get(user_id = request.user.id)
+        if request.method == 'GET':
+            serializer = CustomerSerializer(customer)
+            return Response(serializer.data)
+        
+        elif request.method == 'PUT':
+            serializer = CustomerSerializer(customer, data=request.data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response(serializer.data)
