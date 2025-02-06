b61c7b6406ec9582972de0cbdad4ee8f502829a6
Dev, Omar Abul-gheit <dev.omarabulgheit@gmail.com>
2025-02-06 17:35:15 +0200

    Modified the <OrderItem> model.

--- store/models.py
+++ store/models.py
@@ -128,11 +128,5 @@ class Order(models.Model):
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.PROTECT, related_name='items')
     product = models.ForeignKey(Product, on_delete=models.PROTECT, related_name='orderitems')
+    unit_price = models.DecimalField(max_digits=10, decimal_places=2)
     quantity = models.PositiveSmallIntegerField()
-
-    @property
-    def unit_price(self):
-        return self.product.price
-
-    def __str__(self):
-        return f"{self.product.name}"
